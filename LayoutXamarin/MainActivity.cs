8c6b726118b0f2372229afec6dcc9ef41e623398
Experts <Experts@MASTER203-6>
2023-04-24 11:20:04 +0500

    db working

--- LayoutXamarin/MainActivity.cs
+++ LayoutXamarin/MainActivity.cs
@@ -1,20 +1,56 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using SQLite;
+using System.Collections.Generic;
+using System.IO;
 
 namespace LayoutXamarin
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private ListView listView;
+        private ArrayAdapter<Person> adapter;
+        private List<Person> students;
+        private string path;
+        private SQLiteConnection db;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "mydb.db3");
+            db = new SQLiteConnection(path);
+            db.CreateTable<Person>();
+            db.Insert(new Person { LastName = "Виталий", FirstName = "Филин", Specialty = "Информационные системы и программирование", Group = "320", Email = "easychno@aol.com" });
+
+            students = db.Table<Person>().ToList();
+
+            adapter = new ArrayAdapter<Person>(
+                this,
+                Android.Resource.Layout.SimpleListItem1,
+                students);
+
+            listView = FindViewById<ListView>(Resource.Id.listView);
+            listView.Adapter = adapter;
+
+            var buttonClearDatabase = FindViewById<Button>(Resource.Id.staff);
+            buttonClearDatabase.Click += ButtonClearDatabase_Click;
         }
+
+        private void ButtonClearDatabase_Click(object sender, System.EventArgs e)
+        {
+            db.DeleteAll<Person>();
+            students.Clear();
+            adapter.NotifyDataSetChanged();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
---
+++ LayoutXamarin/Person1.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LayoutXamarin
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Specialty { get; set; }
+        public string Group { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {Specialty} {Email} ({Group})";
+        }
+    }
+
+
+
+}
